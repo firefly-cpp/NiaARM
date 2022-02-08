8a876c4d372b7133d320c972b31b0b70705e75ab
firefly-cpp <iztok@iztok-jr-fister.eu>
2022-02-08 21:56:36 +0100

    Update tests with new scenarios

---
+++ niaarm/tests/test_coverage.py
@@ -0,0 +1,34 @@
+from unittest import TestCase
+from niaarm.association_rule import AssociationRule
+from niaarm.dataset import Dataset
+
+class TestCoverage(TestCase):
+    # let's borrow test case from wikipedia: https://en.wikipedia.org/wiki/Lift_(data_mining)
+
+    def setUp(self):
+        data = Dataset("datasets/wiki_test_case.csv")
+        self.features = data.get_features()
+
+    def test_a(self):
+        # Rule: A => 0
+        antecedent = [['A']]
+
+        consequence = [[0, 0]]
+
+        oper = AssociationRule(self.features)
+
+        coverage = oper.calculate_coverage(antecedent, consequence)
+
+        self.assertEqual(coverage, 1)
+
+    def test_b(self):
+        # Rule: NO => 0
+        antecedent = ["NO"]
+
+        consequence = [[0, 0]]
+
+        oper = AssociationRule(self.features)
+
+        coverage = oper.calculate_coverage(antecedent, consequence)
+
+        self.assertEqual(coverage, 0.5)
--- niaarm/tests/test_support_confidence.py
+++ niaarm/tests/test_support_confidence.py
@@ -4,36 +4,65 @@ from niaarm.dataset import Dataset
 
 class TestSupportConfidence(TestCase):
     # let's borrow test case from wikipedia: https://en.wikipedia.org/wiki/Lift_(data_mining)
-    def test_a(self):
+
+    def setUp(self):
         data = Dataset("datasets/wiki_test_case.csv")
+        self.features = data.get_features()
+        self.transactions = data.transaction_data
 
-        features = data.get_features()
-
-        transactions = data.transaction_data
-
+    def test_a(self):
+        # Rule: A => 0
         antecedent_a = [['A']]
 
         consequence_a = [[0, 0]]
 
-        # TODO: manual checks should be done!
         support_a = 0.42857142857142855
 
         confidence_a = 0.75
 
         vector =  [0.27989089, 0.0, 0.28412449, 0.75629334, 0.0796189,  0.0, 0.0]
 
-        oper = AssociationRule(features)
+        oper = AssociationRule(self.features)
 
-        cut = oper.get_cut_point(0, len(features))
+        cut = oper.get_cut_point(0, len(self.features))
 
         rule = oper.build_rule(vector)
 
         antecedent, consequence = oper.get_ant_con(rule, cut)
 
         support, confidence = oper.calculate_support_confidence(
-                antecedent, consequence, transactions)
+                antecedent, consequence, self.transactions)
 
         self.assertEqual(antecedent, antecedent_a)
         self.assertEqual(consequence, consequence_a)
         self.assertEqual(support_a, support)
         self.assertEqual(confidence_a, confidence)
+
+
+    def test_B(self):
+        # Rule: B => 1
+        antecedent_b = [['B']]
+
+        consequence_b = [[1, 1]]
+
+        support_b = 0.2857142857142857
+
+        confidence_b = 0.666666666667
+
+        vector =  [0.95157038, 0.17362622, 1.0, 0.34473467, 0.65286096, 0.22928163, 0.68833485]
+
+        oper = AssociationRule(self.features)
+
+        cut = oper.get_cut_point(0, len(self.features))
+
+        rule = oper.build_rule(vector)
+
+        antecedent, consequence = oper.get_ant_con(rule, cut)
+
+        support, confidence = oper.calculate_support_confidence(
+                antecedent, consequence, self.transactions)
+
+        self.assertEqual(antecedent, antecedent_b)
+        self.assertEqual(consequence, consequence_b)
+        self.assertEqual(support_b, support)
+        self.assertAlmostEqual(confidence_b, confidence)
