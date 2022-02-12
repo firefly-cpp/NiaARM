86dcf66f7b7ec3a9b80c99cfe0451d3c8fc20004
firefly-cpp <iztok@iztok-jr-fister.eu>
2022-02-12 15:15:27 +0100

    Update test

--- niaarm/tests/test_support_confidence.py
+++ niaarm/tests/test_support_confidence.py
@@ -2,8 +2,10 @@ from unittest import TestCase
 from niaarm.association_rule import AssociationRule
 from niaarm.dataset import Dataset
 
+
 class TestSupportConfidence(TestCase):
-    # let's borrow test case from wikipedia: https://en.wikipedia.org/wiki/Lift_(data_mining)
+    # let's borrow test case from wikipedia:
+    # https://en.wikipedia.org/wiki/Lift_(data_mining)
 
     def setUp(self):
         data = Dataset("datasets/wiki_test_case.csv")
@@ -20,7 +22,14 @@ class TestSupportConfidence(TestCase):
 
         confidence_a = 0.75
 
-        vector =  [0.27989089, 0.0, 0.28412449, 0.75629334, 0.0796189,  0.0, 0.0]
+        vector = [
+            0.27989089,
+            0.10,
+            0.28412449,
+            0.25629334,
+            0.0796189,
+            0.0,
+            0.0]
 
         oper = AssociationRule(self.features)
 
@@ -31,14 +40,12 @@ class TestSupportConfidence(TestCase):
         antecedent, consequence = oper.get_ant_con(rule, cut)
 
         support, confidence = oper.calculate_support_confidence(
-                antecedent, consequence, self.transactions)
-        
-        # TODO
-        self.assertEqual(antecedent, antecedent_a)
-        #self.assertEqual(consequence, consequence_a)
-        #self.assertEqual(support_a, support)
-        #self.assertEqual(confidence_a, confidence)
+            antecedent, consequence, self.transactions)
 
+        self.assertEqual(antecedent, antecedent_a)
+        self.assertEqual(consequence, consequence_a)
+        self.assertEqual(support_a, support)
+        self.assertEqual(confidence_a, confidence)
 
     def test_B(self):
         # Rule: B => 1
@@ -50,7 +57,14 @@ class TestSupportConfidence(TestCase):
 
         confidence_b = 0.666666666667
 
-        vector =  [0.95157038, 0.17362622, 1.0, 0.34473467, 0.65286096, 0.22928163, 0.68833485]
+        vector = [
+            0.95157038,
+            0.17362622,
+            0.8,
+            0.84473467,
+            0.15286096,
+            0.22928163,
+            0.68833485]
 
         oper = AssociationRule(self.features)
 
@@ -61,7 +75,7 @@ class TestSupportConfidence(TestCase):
         antecedent, consequence = oper.get_ant_con(rule, cut)
 
         support, confidence = oper.calculate_support_confidence(
-                antecedent, consequence, self.transactions)
+            antecedent, consequence, self.transactions)
 
         self.assertEqual(antecedent, antecedent_b)
         self.assertEqual(consequence, consequence_b)
