76666bf0c75f34b2844dbb0aea39fc6c4c8ecca1
zStupan <ziga.stupan@gmail.com>
2022-03-01 19:51:01 +0100

    Added a command line interface

---
+++ niaarm/__main__.py
@@ -0,0 +1,6 @@
+import sys
+from niaarm import cli
+
+
+if __name__ == '__main__':
+    sys.exit(cli.main())
---
+++ niaarm/cli.py
@@ -0,0 +1,133 @@
+import argparse
+import os
+from pathlib import Path
+import platform
+import subprocess
+import sys
+import tempfile
+
+import numpy as np
+from niaarm import NiaARM, Dataset, Stats
+from niapy.task import OptimizationType, Task
+from niapy.util.factory import get_algorithm
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(description='Perform ARM, output mined rules as csv, get mined rules\' statistics')
+    parser.add_argument('-a', '--algorithm', type=str, required=True,
+                        help='Algorithm to use (niapy class name, e. g. DifferentialEvolution)')
+    parser.add_argument('-s', '--seed', type=int, help='Seed for thr algorithm\'s random number generator')
+    parser.add_argument('--max-evals', type=int, default=np.inf, help='Maximum number of fitness function evaluations')
+    parser.add_argument('--max-iters', type=int, default=np.inf, help='Maximum number of iterations')
+    parser.add_argument('-i', '--input-file', type=str, required=True, help='Input file containing a csv dataset')
+    parser.add_argument('-o', '--output-file', type=str, help='Output file for mined rules')
+    parser.add_argument('--alpha', type=float, default=0.0, help='Alpha parameter')
+    parser.add_argument('--beta', type=float, default=0.0, help='Beta parameter')
+    parser.add_argument('--gamma', type=float, default=0.0, help='Gamma parameter')
+    parser.add_argument('--delta', type=float, default=0.0, help='Delta parameter')
+    parser.add_argument('--logging', action='store_true', help='Enable logging of fitness improvements')
+    parser.add_argument('--show-stats', action='store_true', help='Display stats about mined rules')
+
+    return parser
+
+
+def text_editor():
+    return os.getenv('VISUAL') or os.getenv('EDITOR') or ('notepad' if platform.system() == 'Windows' else 'vi')
+
+
+def parameters_string(parameters):
+    params_txt = '# You can edit the algorithm\'s parameter values here\n' \
+                 '# Save and exit to continue\n' \
+                 '# WARNING: Do not edit parameter names\n'
+    for parameter, value in parameters.items():
+        params_txt += f'{parameter} = {value}\n'
+    return params_txt
+
+
+def parse_parameters(text):
+    lines: list[str] = text.strip().split('\n')
+    lines = [line.strip() for line in lines if line.strip() and not line.strip().startswith('#')]
+    parameters = {}
+    for line in lines:
+        key, value = line.split('=')
+        key = key.strip()
+        value = float(value.strip())
+        if value.is_integer():
+            value = int(value)
+        parameters[key] = value
+    return parameters
+
+
+def edit_parameters(parameters):
+    parameters.pop('individual_type', None)
+    parameters.pop('initialization_function', None)
+    fd, filename = tempfile.mkstemp()
+    os.close(fd)
+
+    new_parameters = None
+    try:
+        path = Path(filename)
+        path.write_text(parameters_string(parameters))
+        command = f'{text_editor()} {filename}'
+        subprocess.run(command, shell=True, check=True)
+        params_txt = path.read_text()
+        new_parameters = parse_parameters(params_txt)
+    finally:
+        try:
+            os.unlink(filename)
+        except Exception as e:
+            print(e)
+    return new_parameters
+
+
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+    if args.max_evals == np.inf and args.max_iters == np.inf:
+        print('--max-evals and/or --max-iters missing', file=sys.stderr)
+        return 1
+
+    try:
+        dataset = Dataset(args.input_file)
+        problem = NiaARM(dataset.dimension, dataset.features, dataset.transactions, args.alpha, args.beta, args.gamma,
+                         args.delta, args.logging)
+        task = Task(problem, max_iters=args.max_iters, max_evals=args.max_evals,
+                    optimization_type=OptimizationType.MAXIMIZATION)
+
+        algorithm = get_algorithm(args.algorithm, seed=args.seed)
+        params = algorithm.get_parameters()
+        new_params = edit_parameters(params)
+        if new_params is None:
+            print('Invalid parameters', sys.stderr)
+            return 1
+
+        for param in new_params:
+            if param not in params:
+                print(f'Invalid parameter: {param}', sys.stderr)
+                return 1
+
+        algorithm.set_parameters(**new_params)
+
+        algorithm.run(task)
+
+        if args.output_file:
+            problem.export_rules(args.output_file)
+
+        if args.show_stats:
+            stats = Stats(problem.rules)
+            print('\nSTATS:')
+            print(f'Total rules: {stats.total_rules}')
+            print(f'Average fitness: {stats.avg_fitness}')
+            print(f'Average support: {stats.avg_support}')
+            print(f'Average confidence: {stats.avg_confidence}')
+            print(f'Average coverage: {stats.avg_coverage}')
+            print(f'Average shrinkage: {stats.avg_shrinkage}')
+            print(f'Average length of antecedent: {stats.avg_ant_len}')
+            print(f'Average length of consequent: {stats.avg_con_len}')
+
+    except Exception as e:
+        print(e.args, sys.stderr)
+        return 1
