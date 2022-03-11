2164b383e3cfc2131c3d24caa4747fa458c4b9ce
zStupan <ziga.stupan@gmail.com>
2022-03-11 01:40:47 +0100

    Add custom __repr__ method

--- niaarm/feature.py
+++ niaarm/feature.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from typing import Optional
 
 
-@dataclass
+@dataclass(repr=False)
 class Feature:
     r"""Class representing a feature.
 
@@ -11,7 +11,7 @@ class Feature:
        dtype (str): Datatype of the feature.
        min_val (Optional[float]): Minimum value of the feature in the transaction database.
        max_val (Optional[float]): Maximum value of the feature in the transaction database.
-       categories (Optional[list[float]]): Possible categorical feature's values.
+       categories (Optional[list[str]]): Possible categorical feature's values.
 
     """
 
@@ -19,4 +19,15 @@ class Feature:
     dtype: str
     min_val: Optional[float] = None
     max_val: Optional[float] = None
-    categories: Optional[list[float]] = None
+    categories: Optional[list[str]] = None
+
+    def __repr__(self):
+        string = f'{self.name}('
+        if self.dtype == 'cat':
+            string += f'{self.categories if len(self.categories) != 1 else self.categories[0]})'
+        else:
+            if self.min_val == self.max_val:
+                string += f'{self.min_val})'
+            else:
+                string += f'[{self.min_val}, {self.max_val}])'
+        return string
