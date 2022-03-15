e5044f7ea10b477fb8ef343944fae50a5e8c6abb
firefly-cpp <iztok@iztok-jr-fister.eu>
2022-03-15 11:46:25 +0100

    import niaarm when building docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,9 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
 
+# make imports
+from niaarm import NiaARM
+from niaarm.dataset import Dataset
 
 # -- Project information -----------------------------------------------------
 
