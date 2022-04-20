4396b102ad4f728425c3ddf2f8a012d81badb668
zStupan <ziga.stupan@gmail.com>
2022-04-20 08:52:13 +0200

    Rename tdf to hill_slopes

--- niaarm/__init__.py
+++ niaarm/__init__.py
@@ -4,7 +4,7 @@ from niaarm.rule import Rule
 from niaarm.feature import Feature
 from niaarm.mine import get_rules
 from niaarm.rule_list import RuleList
-from niaarm.visualize import tdf
+from niaarm.visualize import hill_slopes
 
 
 __all__ = ['NiaARM', 'Dataset', 'Feature', 'Rule', 'RuleList', 'get_rules']
