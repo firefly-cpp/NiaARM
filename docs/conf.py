df7e0d368f4e1f378cf664a510bd584aa2d26957
firefly-cpp <iztok@iztok-jr-fister.eu>
2022-09-29 10:41:41 +0200

    Bump version to 0.2.1

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ copyright = '2022, Žiga Stupan, Iztok Fister Jr.'
 author = 'Žiga Stupan, Iztok Fister Jr.'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '0.2.1'
 
 
 # -- General configuration ---------------------------------------------------
