8073dda17672bfcfda7056f844de9972cb2940dc
firefly-cpp <iztok@iztok-jr-fister.eu>
2022-12-17 09:08:47 +0100

    Bump version

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ copyright = '2022, Žiga Stupan, Iztok Fister Jr.'
 author = 'Žiga Stupan, Iztok Fister Jr.'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.2'
+release = '0.2.3'
 
 
 # -- General configuration ---------------------------------------------------
--- niaarm/__init__.py
+++ niaarm/__init__.py
@@ -9,4 +9,4 @@ from niaarm.visualize import hill_slopes
 
 __all__ = ['NiaARM', 'Dataset', 'Feature', 'Rule', 'RuleList', 'get_rules']
 
-__version__ = "0.2.2"
+__version__ = "0.2.3"
